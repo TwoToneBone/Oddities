f0bf36fa51e3120c46e88dd771b9dd9dbce6267f
Erik <erik.ra@gmail.com>
2017-04-07 00:40:21 +0200

    init

--- Oddities/Program.cs
+++ Oddities/Program.cs
@@ -10,6 +10,20 @@ namespace Oddities
     {
         static void Main(string[] args)
         {
+            List<string> outputs = new List<string>();
+            int loops = int.Parse(Console.ReadLine());
+            for (int i = 0; i < loops; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                string @out = Math.Abs(input % 2) == 0 ? $"{input} is even" : $"{input} is odd";
+
+                outputs.Add(@out);
+            }
+            foreach (var s in outputs)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
